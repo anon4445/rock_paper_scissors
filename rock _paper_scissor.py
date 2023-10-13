4618d650e84f66e80d970b317667fae5d52686bd
anon4445 <99899913+anon4445@users.noreply.github.com>
2023-10-13 18:57:59 +0600

    add rock_paper__scissor.py

---
+++ rock _paper_scissor.py
@@ -0,0 +1,30 @@
+import random
+
+def play_game():
+    choices = ['rock', 'paper', 'scissors']
+
+    while True:
+        user_choice = input("Enter your choice (rock, paper, or scissors): ").lower()
+
+        if user_choice not in choices:
+            print("Invalid choice. Please try again.")
+            continue
+
+        computer_choice = random.choice(choices)
+        print("Computer's choice:", computer_choice)
+
+        if user_choice == computer_choice:
+            print("It's a tie!")
+        elif (user_choice == 'rock' and computer_choice == 'scissors') or \
+             (user_choice == 'paper' and computer_choice == 'rock') or \
+             (user_choice == 'scissors' and computer_choice == 'paper'):
+            print("You win!")
+        else:
+            print("You lose!")
+
+        play_again = input("Do you want to play again? (yes/no): ").lower()
+        if play_again != 'yes':
+            break
+
+if __name__ == "__main__":
+    play_game()
